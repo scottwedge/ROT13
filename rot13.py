8b11d02709960b4b6269d120fd2ad693d02e90e8
Scott Wedge <scott.wedge@gmail.com>
2022-04-11 22:50:01 -0400

    Start coding

--- rot13.py
+++ rot13.py
@@ -4,4 +4,19 @@
 # Cryptography is fun
 # Pelcgbtencul vf sha
 
+# Functions:
+
+def encode(text, up, low):
+    result = ""
+    for j in text:
+        if j in up:
+            pass
+        if j in low:
+            pass
+    return result
+
+upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
 
